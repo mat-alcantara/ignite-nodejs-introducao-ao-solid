b6b3d58c5074513d95b405100d16f104705302a1
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-10-01 22:00:01 -0300

    Finish documentation challenge

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import SwaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerConfig));
 
 app.use("/users", usersRoutes);
 
